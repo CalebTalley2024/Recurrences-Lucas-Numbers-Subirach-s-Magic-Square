178cbdf431a8147c8d094d6277a6aa988bbef3da
Caleb Talley <86822049+CalebTalley2024@users.noreply.github.com>
2022-11-14 23:09:50 -0500

    Most of 3 is done, but there are still some problems - still have to do 3e, bonus questions, and setting everything up for parts 1 and 2 - Power set function size is off of what it should be (2^k) - Hash maps functions for 3 are out of bounds and/or not working

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
         // Part 2a
-        int n = 0;
-        while( n<30){
-            //Sequence.LucinacciRatioCalc(n);
-            Sequence.myAnacciRatioTime(30);
-            n++;
-        }
+//        int n = 0;
+//        while( n<30){
+//            //Sequence.LucinacciRatioCalc(n);
+//            Sequence.myAnacciRatioTime(30);
+//            n++;
+//        }
 //        Sequence.LucinacciRatioCalc(40);
 //        Sequence.LucinacciRatioTime(40);
 //        System.out.println(myAnacci(30));
 //        myAnacciRatioCalc(20);
 //        Sequence.myAnacciRatioTime(30);
 //          myAnacciTime(20);
+        // Part 3a
+        // test for power set function
+
+//        System.out.println(Square.getPowerSet(Square.magicNums));
+//        System.out.println(Square.MagicSubSetFour(Square.magicNums));
+//
+//        System.out.println(Square.allMagicSubSets(Square.magicNums));
+//        System.out.println(IntStream.of(Square.magicNums).sum());
+        System.out.println(Square.getSumsHashMap(Square.magicNums).get(33));
+
+
 
     }
+
 }
 
 /*
@@ -32,3 +49,4 @@ public class Main {
 * Plan
 * make algorithm that gets all of the combinations, then filter with for loops and hashmaps
 * */
+
--- src/Square.java
+++ src/Square.java
@@ -1,37 +1,136 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Square {
     // this is an array of all of the values for the Subirach Magic Square
     ArrayList<Integer> allNumbers = new ArrayList<Integer>(
-            Arrays.asList(1,14,14,4,11,7,6,9,8,10,10,5));
-
-
+            Arrays.asList(1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5,13,2,3,15));
     // magic sum is what each column, row, etc add up to (1 + 14 + 14 + 4)
     int magicSum = 33;
+    public static int[] magicNums = {1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5,13,2,3,15};
 
-    // 3a Find all 4-element combinations that have the same sum as magicSum
+    /*
+     * Takes in int array set
+     * outputs the power set of our inputed set
+     *
+     * */
+    public static ArrayList<ArrayList<Integer>> getPowerSet(int[] nums) {
+        ArrayList<ArrayList<Integer>> powerSet = new ArrayList<>();
+        // sort the number array for readability and to account for duplicates
+        Arrays.sort(nums);
+        // this function will recursively make all of our subsets
+        backtrack(powerSet, new ArrayList<>(), nums, 0);
+        return powerSet;
+    }
 
-    // gives you all of the subsets in the array
-    public ArrayList<Integer> powerset(int n){
-        // get all of the numbers
-        ArrayList<Integer> allNumbers = this.allNumbers;
+    /*
+     * recursive function that gets the powerset of a set
+     * takes in list of subsets, a slot for making subsets, a set to make subsets from, and a starting index
+     * */
+    public static void backtrack(ArrayList<ArrayList<Integer>> powerSet, ArrayList<Integer> tempSlot, int[] nums, int start) {
+        // adding subsets to our powerset list
+        powerSet.add(new ArrayList<>(tempSlot));
+//        System.out.println(tempSlot);
+        // iterate through all possible numbers
+        // for head index of a loop, you will get all of the subsets that contain the value at index i
+        // the algorithm will not repeat the same subsets (unless there are two of the same value)
+        for (int i = start; i < nums.length; i++) {
+            /** make sure that we only have the same elements in a subset when the start is i is at the start position
+             (whether in the first for loop or a recursive for loop)*/
+            if (i > start && nums[i] == nums[i - 1]) {
+                // if we have a duplicate that is not at the start of a function call, skip this subset
+                continue;
+            } else {
+                // adding numbers to our subset creater: tempSlot
+                tempSlot.add(nums[i]);
+//            System.out.println(tempSlot);
+                //recursive call to make all of the subsets that contain the value at index i
+                backtrack(powerSet, tempSlot, nums, i + 1);
+//            System.out.println("number to be removed: " + tempSlot.get(tempSlot.size()-1));
+                // When at the end of the recursive call, remove the last value in our temporary subset slot
+                tempSlot.remove(tempSlot.size() - 1);
+            }
+        }
+    }
 
-        // make a stack to place items
-        Stack<Integer> numStack = new Stack<Integer>();
-
-        for (int i = 0; i<allNumbers.size(); i++){
-            // set your node
-            int node = allNumbers.get(i);
-            // remove node from list
-            allNumbers.remove(i);
 
 
+    // helper function for summing up all of the elements in a list
+    public static int getSum(ArrayList<Integer> subset){
+        int sum = 0;
+        for(int aNum:subset){
+            sum += aNum;
+        }
+        return sum;
+    }
+    /////////// Part 3
+    // 3a
+// outputs all of the elements that are 4 elements long that sum up to 33
+    public static ArrayList<ArrayList<Integer>> MagicSubSetFour(int[] nums) {
+        // get the power set of elements
+        ArrayList<ArrayList<Integer>> powerSet = Square.getPowerSet(nums);
+        // get rid of alls  possible sub sets that are NOT 4 elements long
+        powerSet.removeIf(set -> (set.size() != 4));
+        // get rid of all sub sets that do NOT sum up to 33
+        powerSet.removeIf(set -> (Square.getSum(set) !=33));
+        return powerSet;
+    }
+    // 3b: There are 35 subsets that sum up to 33
+    //
+    public static int allMagicSubSets(int[] nums) {
+        // get the power set of elements
+        ArrayList<ArrayList<Integer>> powerSet = Square.getPowerSet(nums);
 
+        // get rid of all sub sets that do NOT sum up to 33
+        powerSet.removeIf(set -> (Square.getSum(set) !=33));
+        return powerSet.size();
+    }
+
+
+
+    //
+    // 3c: find all the possible sums for each sub set in the power set
+
+    public static HashMap<Integer,ArrayList<ArrayList<Integer>>> getSumsHashMap(int[]nums){
+        // get the powerset from our nums
+        ArrayList<ArrayList<Integer>> powerSet = Square.getPowerSet(nums);
+        // Use a hashmap
+        //Key: the sum value
+        // Value: the subsets that sum up to our key/sum value
+        HashMap<Integer,ArrayList<ArrayList<Integer>>> sumHashMap = new HashMap<Integer,ArrayList<ArrayList<Integer>>>();
+
+        // our lowest sum will be 0
+        // our largest sum will be the sum of everything in our set
+        int maxSum = IntStream.of(nums).sum();
+        //associate all values with correct keys
+        ArrayList<ArrayList<Integer>> powerSetSumValue = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i<maxSum;i++){
+            int key = i;
+            // make a copy of your power set and filter subsets that sum up to your key index
+
+//            powerSetSumValue = powerSet;
+//            powerSetSumValue.removeIf(set -> (Square.getSum(set) != key));
+            sumHashMap.put(i,sumValidSubSets(i,powerSet));
+        }
+        /// RETURN HASHMAP THat actually have values
+        return sumHashMap;
+    }
+    public static ArrayList<ArrayList<Integer>> sumValidSubSets(int sum,ArrayList<ArrayList<Integer>> powerSetSumValue){
+
+        ArrayList<ArrayList<Integer>> validSubSets = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i<powerSetSumValue.size();i++){
+            ArrayList<Integer> subset = validSubSets.get(i);
+            if(getSum(subset) == sum){
+                validSubSets.add(subset);
+            }
 
         }
+        return validSubSets;
 
     }
 
+
 }
+
+
+
